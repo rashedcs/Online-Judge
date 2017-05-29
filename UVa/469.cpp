019d7bec11662a0f823ee4b4aa6a262cffd696aa
Rashed <rashedcs@users.noreply.github.com>
2017-05-29 17:35:11 +0600

    Update 469.cpp

--- UVa/469.cpp
+++ UVa/469.cpp
@@ -6,7 +6,7 @@ char grid[100][100];
 int vis[100][100];
 
 
-int dr[] = {1,1,0,-1,-1,-1,0,1}; 
+int dr[] = {1,1,0,-1,-1,-1,0,1};
 int dc[] = {0,1,1, 1, 0,-1,-1,-1};
 
 
@@ -32,7 +32,7 @@ void bfs(int n, int m, int sx, int sy)
     {
         int r = Q.front().first;
         int c = Q.front().second;
-        
+
         Q.pop();
         sum++;
 
@@ -52,47 +52,47 @@ void bfs(int n, int m, int sx, int sy)
 }
 
 
-int main() 
+int main()
 {
     ios::sync_with_stdio(false);
     int tc, n, m, sx, sy;
     char str[105];
-    
+
     scanf("%d ", &tc);
-    while(tc--) 
+    while(tc--)
     {
          memset(grid, 0, sizeof(grid));
          int p = 0;
-      
-         while(gets(str)&&str[0]!='\0')
+
+         while(gets(str) && strlen(str)>0)
          {
-            // if(str[0] == '\0')     break;
-            if(str[0] != 'W' && str[0] != 'L') 
+
+            if (str[0] == 'L' || str[0] == 'W')
+            {
+                 sscanf(str, "%s", grid[p++]);
+            }
+        
+            else
             {
                 sscanf(str, "%d %d", &sx, &sy);
                 memset(vis, 0, sizeof(vis));
-                
+
                 sum = 0;
-                
+
                 n =  sizeof grid/sizeof (grid[0]);
-                m =  sizeof grid[0]/sizeof grid[0][0]; 
-                
+                m =  sizeof grid[0]/sizeof grid[0][0];
+
                 bfs(n, m, sx-1, sy-1);
-                
+
                 printf("%d\n",sum);
-             } 
-             else 
-             {
-                sscanf(str, "%s", grid[p++]);
              }
+
          }
-     if(tc>0)  printf("\n");
+      if(tc>0)  printf("\n");
     }
-  
+
     return 0;
 }
-
-
 
 
 
