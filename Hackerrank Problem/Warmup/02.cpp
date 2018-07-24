bbf3b66a1c7b910b379b932d7c225592220a76f0
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-07-24 07:17:43 +0600

    Simple Array Sum

---
+++ Hackerrank Problem/Warmup/02.cpp
@@ -0,0 +1,18 @@
+/* Problem Name : Simple Array Sum */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    cin>>n;
+    int arr[n+1];
+    
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+        sum =sum + arr[i];
+    }
+    cout<<sum<<endl;
+}
