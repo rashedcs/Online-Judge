2c98ecf3897a40738a3fbb7fdc7bd6769e1c0324
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-07-24 07:15:59 +0600

    Solve Me First

---
+++ Hackerrank Problem/Warmup/01.cpp
@@ -0,0 +1,15 @@
+/* Problem Name : Solve Me First */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int num1, num2;
+    int sum;
+    cin>>num1>>num2;
+    sum = num1 + num2;
+    cout<<sum;
+    return 0;
+}
