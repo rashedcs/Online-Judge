82ad5f6dbdb0331027896d7958feba4f7c18c532
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-08 22:00:35 +0600

    484 - The Department of Redundancy Department

--- UVa/484.cpp
+++ UVa/484.cpp
@@ -4,23 +4,23 @@
 
     	int main(int argc, char const *argv[])
     	{
-	        int n, t;
-	        map<int,int> mp;
+	          int n, t;
+	          map<int,int> mp;
 	        
-	        vector<int> arr; //Map result in sorting but in this problem....unsorted so use extra vector
+	          vector<int> arr; //Map result in sorting but in this problem....unsorted so use extra vector
           
-          while(cin>>t)
-          {
-              mp[t]++; 
-              if (mp[t] == 1)
-              {
-                  arr.push_back(t);
-              }
-          }
+		  while(cin>>t)
+		  {
+		      mp[t]++; 
+		      if (mp[t] == 1)
+		      {
+			  arr.push_back(t);
+		      }
+		  }
     		
-        	for(int i=0; i<arr.size(); i++)
-        	{
+        	  for(int i=0; i<arr.size(); i++)
+        	  {
         	    	cout<<arr[i]<<" "<<mp[arr[i]]<<endl;
-        	}
-    	  	return 0;
+        	  }
+    	  	  return 0;
     	 }
