e388676b3de7171a2b9552599d17976eff3edbe4
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-09 23:08:26 +0600

    575 - Skew Binary

---
+++ UVa/575.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	long long sum, n, len;
+	string s;
+	
+	while(cin>>s && s!="0")
+	{
+        sum = 0;
+        len = s.size();
+
+        for(int i=0; s[i]; i++)
+        {
+            n = s[i]-'0';
+            sum += n*(pow(2,(len-i))-1);
+        }
+        cout<<sum<<endl;
+	 }
+	return 0;
+}
