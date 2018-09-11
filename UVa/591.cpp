6200802f253d2acdf8a1299cb5b071651e1364f2
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-11 21:43:01 +0600

    591 - Box of Bricks

---
+++ UVa/591.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int mean(int arr[], int n)
+{
+        int  sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += arr[i];
+        }
+        return sum/n;
+}
+
+
+int main()
+{
+    int n, avg, mov, cnt=0;
+    int arr[60];
+    
+    while(cin>>n && n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        
+        avg = mean(arr, n);
+
+        mov = 0;
+        
+        printf("Set #%d\n", ++cnt);
+        
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] > avg)
+            {
+                mov += arr[i] - avg;
+            }
+        }
+        printf("The minimum number of moves is %d.\n\n",mov);
+
+    }
+    return 0;
+}
